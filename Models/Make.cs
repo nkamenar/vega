80e21ad4c5d2f6ad1422b0050af6553ddadc9e6a
Nathan Kamenar <nkamenar.it@gmail.com>
2018-07-03 23:40:06 -0400

    Set up DbContext and made initial migration

---
+++ Models/Make.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace vega.Models
+{
+    public class Make
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Model> Models { get; set; } = new Collection<Model>();
+    }
+}
